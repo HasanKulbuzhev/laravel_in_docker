5f98c011ad65bc1a5259adf1bf6dfa15e3b3eac4
Hasan <ashatkfhcj@gmail.com>
2021-05-07 21:34:19 +0300

    Добавил поддержку вывода questions по user_id

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($post) {
+            $post->{$post->getKeyName()} = (string) Str::uuid();
+        });
+    }
+    public function getIncrementing()
+    {
+        return false;
+    }
+    public function getKeyType()
+    {
+        return 'string';
+    }
 }
